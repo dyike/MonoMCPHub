10239d82685df3290a325ae77040339f0f800c17
ityike <yuanfeng634@gmail.com>
2025-04-18 21:47:39 +0800

    test mcp sse

--- cmd/browser-mcp/main.go
+++ cmd/browser-mcp/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"log"
 	"log/slog"
 	"os"
 
@@ -9,7 +12,21 @@ import (
 	"github.com/mark3labs/mcp-go/server"
 )
 
+var (
+	transport string
+	port      string
+)
+
+func init() {
+	flag.StringVar(&transport, "t", "stdio", "Transport type (stdio or sse)")
+	flag.StringVar(&transport, "transport", "stdio", "Transport type (stdio or sse)")
+	flag.StringVar(&port, "p", "8080", "Port to listen on")
+	flag.StringVar(&port, "port", "8080", "Port to listen on")
+}
+
 func main() {
+	flag.Parse()
+
 	logLevel := slog.LevelDebug
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
 		Level: logLevel,
@@ -30,8 +47,16 @@ func main() {
 
 	s.AddTools(bs.Tools()...)
 
-	if err := server.ServeStdio(s); err != nil {
-		slog.Error("Failed to serve", "error", err)
-		os.Exit(1)
+	if transport == "sse" {
+		sseServer := server.NewSSEServer(s)
+		if err := sseServer.Start(fmt.Sprintf(":%s", port)); err != nil {
+			log.Fatalf("Server error: %v", err)
+		}
+
+	} else {
+		if err := server.ServeStdio(s); err != nil {
+			slog.Error("Failed to serve", "error", err)
+			os.Exit(1)
+		}
 	}
 }
